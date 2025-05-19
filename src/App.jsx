1dd611de6bd31a7c6c5794247522df37e2a4b3c0
Prateek Haldkar <137743180+prateekhaldkar@users.noreply.github.com>
2025-05-20 01:45:56 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import EventCalendar from './components/EventCalendar'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-500">
+    <div className="min-h-screen bg-gray-50">
       <EventCalendar />
       
     </div>
